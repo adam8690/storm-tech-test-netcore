d70852ea107f337d15532dc48b56c5d01e7571a0
Adam Baxter <adam@pop-os.localdomain>
2020-05-17 16:15:27 +0100

    5 use displayname instead of hard coded string.

--- Todo/Models/TodoItems/TodoItemEditFields.cs
+++ Todo/Models/TodoItems/TodoItemEditFields.cs
@@ -1,4 +1,5 @@
-﻿using Todo.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using Todo.Data.Entities;
 
 namespace Todo.Models.TodoItems
 {
@@ -8,7 +9,8 @@ namespace Todo.Models.TodoItems
         public string Title { get; set; }
         public string TodoListTitle { get; set; }
         public int TodoItemId { get; set; }
-        public bool IsDone { get; set; }
+        public bool IsDone { get; set; }        
+        [Display(Name = "Responsible Party")]
         public string ResponsiblePartyId { get; set; }
         public Importance Importance { get; set; }
 
